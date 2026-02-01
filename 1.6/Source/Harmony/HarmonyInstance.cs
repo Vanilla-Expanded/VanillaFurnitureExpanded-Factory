a92898de15bb651a0b262ddd40cee98d18335554
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-02-01 22:11:45 +0100

    Initial commit

---
+++ 1.6/Source/Harmony/HarmonyInstance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using HarmonyLib;
+using System.Reflection;
+namespace VanillaFurnitureExpandedFactory
+{
+    [StaticConstructorOnStartup]
+    public class Main
+    {
+        static Main()
+        {
+            var harmony = new Harmony("com.VanillaFurnitureExpandedFactory");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
