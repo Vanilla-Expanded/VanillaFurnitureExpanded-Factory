51bddb30e7f7c3a0bb4c0bf863d5f5c5331dfb6c
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-02-10 16:27:07 +0100

    v0.1

---
+++ 1.6/Source/GraphicsCache/GraphicsCache.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using Verse;
+
+namespace VanillaFurnitureExpandedFactory
+{
+    [StaticConstructorOnStartup]
+    public static class GraphicsCache
+    {
+        public static readonly Graphic_Multi hopperNormal = (Graphic_Multi)GraphicDatabase.Get<Graphic_Multi>("Things/Building/FactoryHopper/FactoryHopper_Unset", ShaderDatabase.CutoutComplex, new Vector2(1.05f, 1.05f), Color.white);
+
+        public static readonly Graphic_Multi hopperInput = (Graphic_Multi)GraphicDatabase.Get<Graphic_Multi>("Things/Building/FactoryHopper/FactoryHopper_Input", ShaderDatabase.CutoutComplex, new Vector2(1.05f,1.05f), Color.white);
+        public static readonly Graphic_Multi hopperOutput = (Graphic_Multi)GraphicDatabase.Get<Graphic_Multi>("Things/Building/FactoryHopper/FactoryHopper_Output", ShaderDatabase.CutoutComplex, new Vector2(1.05f, 1.05f), Color.white);
+
+    }
+}
---
+++ 1.6/Source/Things/Building_FactoryHopper.cs
@@ -0,0 +1,99 @@
+﻿using PipeSystem;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+
+namespace VanillaFurnitureExpandedFactory
+{
+    public class Building_FactoryHopper : Building_Storage
+    {
+        public Graphic cachedGraphic;
+
+        public Graphic graphic
+        {
+            get
+            {
+                if (cachedGraphic == null)
+                {
+                    cachedGraphic = CheckForFactories();
+                }
+                return cachedGraphic;
+
+            }
+
+        }
+
+        public override void TickLong()
+        {
+            base.TickLong();
+
+            cachedGraphic = CheckForFactories();
+            this.Map.mapDrawer.MapMeshDirty(this.Position, MapMeshFlagDefOf.Things | MapMeshFlagDefOf.Buildings);
+          
+        }
+
+        public Graphic CheckForFactories()
+        {
+            if (this.Map != null)
+            {
+                foreach (IntVec3 edgeCell in this.OccupiedRect().ExpandedBy(1).EdgeCells)
+                {
+                    if (edgeCell.InBounds(this.Map))
+                    {
+                        List<Thing> things = edgeCell.GetThingList(this.Map);
+                        foreach(Thing thing in things)
+                        {
+                            if (thing.def.IsEdifice())
+                            {
+                                CompAdvancedResourceProcessor comp = thing.TryGetComp<CompAdvancedResourceProcessor>();
+                                if (comp != null)
+                                {
+                                    if (thing.def.hasInteractionCell) {
+                                        IntVec3 pos = thing.Position + thing.def.interactionCellOffset.RotatedBy(thing.Rotation);
+                                        if (pos == this.Position)
+                                        {
+                                            return GraphicsCache.hopperOutput;
+                                        }
+
+                                    }
+
+                                    if(comp.Process != null)
+                                    {
+
+                                        if (!comp.Process.Def.autoInputSlots.NullOrEmpty())
+                                        {
+                                            foreach(IntVec3 autoslot in comp.Process.Def.autoInputSlots)
+                                            {
+                                                IntVec3 pos = thing.Position + autoslot.RotatedBy(thing.Rotation);
+                                                if(pos == this.Position)
+                                                {
+                                                    return GraphicsCache.hopperInput;
+                                                }
+                                            }
+                                            
+
+                                        }
+
+                                    }
+                                }
+                            }
+                            
+                        }
+
+                    }
+                }
+            }
+
+
+            return GraphicsCache.hopperNormal;
+        }
+
+        public override Graphic Graphic => graphic;
+
+    }
+}
