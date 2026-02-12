8a1db56561a59118cbb05cf2a3babdb1191cec7c
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-02-12 16:40:10 +0100

    v0.1

---
+++ 1.6/Source/ResultWorkers/ResultWorker_SmeltMechanoid.cs
@@ -0,0 +1,27 @@
+﻿using PipeSystem;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using static PipeSystem.ProcessDef;
+
+namespace VanillaFurnitureExpandedFactory
+{
+    public class ResultWorker_SmeltMechanoid : ResultWorker
+    {
+       
+
+        public override int GetCount(Process process)
+        {
+            ThingDef mechCorpse = process?.GetLastStoredIngredient();
+            if (mechCorpse != null)
+            {        
+                 return (int)(mechCorpse.race?.baseBodySize * 10);          
+            }
+            return result.count;
+        }
+    }
+}
