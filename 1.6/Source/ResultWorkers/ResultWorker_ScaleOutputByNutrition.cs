db7f345bb139d2a48ee4001fd97bea1542448378
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-02-13 08:46:30 +0100

    v0.1

---
+++ 1.6/Source/ResultWorkers/ResultWorker_ScaleOutputByNutrition.cs
@@ -0,0 +1,26 @@
+﻿using PipeSystem;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using static PipeSystem.ProcessDef;
+
+namespace VanillaFurnitureExpandedFactory
+{
+    public class ResultWorker_ScaleOutputByNutrition : ResultWorker
+    {
+        public override int GetCount(Process process)
+        {
+            ThingDef lastFoodItem = process?.GetLastStoredIngredient();
+            if (lastFoodItem != null && lastFoodItem.IsIngestible)
+            {
+             
+                return Math.Max((int)(Math.Round(lastFoodItem.GetStatValueAbstract(StatDefOf.Nutrition) / 0.05f)), 1); 
+            }
+            return result.count;
+        }
+    }
+}
