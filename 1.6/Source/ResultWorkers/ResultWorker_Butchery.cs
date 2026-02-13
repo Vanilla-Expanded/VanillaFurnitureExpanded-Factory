b6a1e7e96f8be2144d6ef524fee57cf7938076ae
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-02-13 13:17:28 +0100

    v0.1

---
+++ 1.6/Source/ResultWorkers/ResultWorker_Butchery.cs
@@ -0,0 +1,26 @@
+﻿using PipeSystem;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using static PipeSystem.ProcessDef;
+
+namespace VanillaFurnitureExpandedFactory
+{
+    public class ResultWorker_Butchery : ResultWorker
+    {
+
+        public override int GetCount(Process process)
+        {
+            ThingDef corpse = process?.GetLastStoredIngredient();
+            if (corpse != null)
+            {
+                return (int)Math.Max(Math.Round(corpse.GetStatValueAbstract(StatDefOf.MeatAmount)),1);
+            }
+            return result.count;
+        }
+    }
+}
---
+++ 1.6/Source/ResultWorkers/ResultWorker_MasonryBlocks.cs
@@ -0,0 +1,28 @@
+﻿using PipeSystem;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using static PipeSystem.ProcessDef;
+
+namespace VanillaFurnitureExpandedFactory
+{
+    public class ResultWorker_MasonryBlocks : ResultWorker
+    {
+
+        public override ThingDef GetResult(Process process)
+        {
+            ThingDef inputSmeltable = process?.GetLastStoredIngredient();
+                      
+            if (inputSmeltable != null && !inputSmeltable.butcherProducts.NullOrEmpty())
+            {
+                return inputSmeltable.butcherProducts.First().thingDef;               
+            }
+
+            return result.thing;
+        }
+    }
+}
