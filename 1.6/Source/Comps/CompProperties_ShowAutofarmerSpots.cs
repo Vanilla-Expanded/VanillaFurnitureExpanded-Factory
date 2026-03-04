15067b67844686bf3bf7121fd2611df2f55fd200
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-03-04 19:06:23 +0100

    Autofarmer colours

---
+++ 1.6/Source/Comps/CompProperties_ShowAutofarmerSpots.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace VanillaFurnitureExpandedFactory
+{
+    public class CompProperties_ShowAutofarmerSpots : CompProperties
+    {
+       
+        public CompProperties_ShowAutofarmerSpots()
+        {
+            compClass = typeof(CompShowAutofarmerSpots);
+        }
+
+      
+    }
+}
---
+++ 1.6/Source/Comps/CompShowAutofarmerSpots.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using PipeSystem;
+using RimWorld;
+using Verse;
+using Verse.Sound;
+
+namespace VanillaFurnitureExpandedFactory
+{
+   
+    public class CompShowAutofarmerSpots : ThingComp
+    {
+        public CompProperties_ShowAutofarmerSpots Props => props as CompProperties_ShowAutofarmerSpots;
+
+        public override void PostDrawExtraSelectionOverlays()
+        {
+
+            ProcessUtility.DrawSlot(parent, new IntVec3(-3,0,-2), GraphicsCache.AutoFarmerGreenMaterial);
+            ProcessUtility.DrawSlot(parent, new IntVec3(-2, 0, -2), GraphicsCache.AutoFarmerYellowMaterial);
+            ProcessUtility.DrawSlot(parent, new IntVec3(-1, 0, -2), GraphicsCache.AutoFarmerBrownMaterial);
+            ProcessUtility.DrawSlot(parent, new IntVec3(0, 0, -2), GraphicsCache.AutoFarmerPurpleMaterial);
+            ProcessUtility.DrawSlot(parent, new IntVec3(1, 0, -2), GraphicsCache.AutoFarmerBlueMaterial);
+            ProcessUtility.DrawSlot(parent, new IntVec3(2, 0, -2), GraphicsCache.AutoFarmerPurpleMaterial);
+            ProcessUtility.DrawSlot(parent, new IntVec3(3, 0, -2), GraphicsCache.AutoFarmerYellowMaterial);
+
+
+        }
+    }
+}
