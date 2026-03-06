61de40ee99695b6f508dbfa1171337b2bf217e8f
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-03-06 13:52:37 +0100

    Drill for random metals

---
+++ 1.6/Source/Defs/DrillableMetalsDef.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Verse;
+
+namespace VanillaFurnitureExpandedFactory
+{
+    
+    public class DrillableMetalsDef : Def
+    {
+        public List<ThingDef> drillableMetals;
+    
+    }
+}
---
+++ 1.6/Source/ResultWorkers/ResultWorker_Metals.cs
@@ -0,0 +1,47 @@
+﻿using PipeSystem;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using static PipeSystem.ProcessDef;
+
+namespace VanillaFurnitureExpandedFactory
+{
+    public class ResultWorker_Metals : ResultWorker
+    {
+        public ThingDef cachedMetal;
+
+        public override ThingDef GetResult(Process process)
+        {
+            List<ThingDef> allMetals = new List<ThingDef>();
+            List<DrillableMetalsDef> allMetalLists = DefDatabase<DrillableMetalsDef>.AllDefsListForReading;
+            foreach (DrillableMetalsDef individualList in allMetalLists)
+            {
+                allMetals.AddRange(individualList.drillableMetals);
+            }
+
+            ThingDef resultingMetal = allMetals.RandomElementByWeight(x => x.deepCommonality);
+
+            if (resultingMetal != null)
+            {
+                cachedMetal = resultingMetal;
+                return resultingMetal;
+            }
+            cachedMetal = ThingDefOf.Steel;
+            return result.thing;
+        }
+
+        public override int GetCount(Process process)
+        {
+            if (cachedMetal != null)
+            {
+                return cachedMetal.deepCountPerPortion;
+
+            }
+            return result.count;
+        }
+    }
+}
